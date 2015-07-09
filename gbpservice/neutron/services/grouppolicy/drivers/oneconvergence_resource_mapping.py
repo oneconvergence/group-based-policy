29baf10434f571986571428a6e836c79592f075f
ashutosh-mishra <ashutosh.mishra@oneconvergence.com>
2015-07-09 14:43:51 +0530

    Modified oc RMD  to disable security group rules if firewall present in service chain.

--- gbpservice/neutron/services/grouppolicy/drivers/oneconvergence_resource_mapping.py
+++ gbpservice/neutron/services/grouppolicy/drivers/oneconvergence_resource_mapping.py
@@ -12,15 +12,12 @@
 # One Convergence, Inc., USA
 
 
-import netaddr
 
 from neutron.common import log
-from neutron.openstack.common import jsonutils
 from neutron.openstack.common import log as logging
 from neutron.plugins.common import constants as pconst
 from neutron.common import exceptions as n_exc
 from neutron.extensions import securitygroup as ext_sg
-from oslo.config import cfg
 
 from gbpservice.neutron.services.grouppolicy.common import constants as gconst
 from gbpservice.neutron.services.grouppolicy.common import exceptions as exc
@@ -53,6 +50,54 @@ class OneConvergenceResourceMappingDriver(resource_mapping.ResourceMappingDriver
         if len(context.current['external_segments']) > 1:
             raise exc.MultipleESPerEPNotSupported()
 
+    @log.log
+    def create_policy_target_group_postcommit(self, context):
+        subnets = context.current['subnets']
+        if subnets:
+            l2p_id = context.current['l2_policy_id']
+            l2p = context._plugin.get_l2_policy(context._plugin_context,
+                                                l2p_id)
+            l3p_id = l2p['l3_policy_id']
+            l3p = context._plugin.get_l3_policy(context._plugin_context,
+                                                l3p_id)
+            router_id = l3p['routers'][0] if l3p['routers'] else None
+            for subnet_id in subnets:
+                self._use_explicit_subnet(context._plugin_context, subnet_id,
+                                          router_id)
+        else:
+            self._use_implicit_subnet(context)
+        self._handle_network_service_policy(context)
+        self._handle_policy_rule_sets(context)
+        self._update_default_security_group(context._plugin_context,
+                                            context.current['id'],
+                                            context.current['tenant_id'],
+                                            context.current['subnets'])
+        if (self._is_firewall_in_sc_spec(context, ptg_id=context.current['id'])
+            and self._is_ptg_provider(context, context.current['id'])):
+            self._create_service_vm_sg(context,
+                                       context.current['tenant_id'],
+                                       context.current['id'])
+
+    @log.log
+    def delete_policy_target_group_precommit(self, context):
+        context.nsp_cleanup_ipaddress = self._get_ptg_policy_ipaddress_mapping(
+            context._plugin_context.session, context.current['id'])
+        context.nsp_cleanup_fips = self._get_ptg_policy_fip_mapping(
+            context._plugin_context.session, context.current['id'])
+        provider_ptg_chain_map = self._get_ptg_servicechain_mapping(
+                                            context._plugin_context.session,
+                                            context.current['id'],
+                                            None)
+        consumer_ptg_chain_map = self._get_ptg_servicechain_mapping(
+                                            context._plugin_context.session,
+                                            None,
+                                            context.current['id'],)
+        context.ptg_chain_map = provider_ptg_chain_map + consumer_ptg_chain_map
+        if (self._is_firewall_in_sc_spec(context, ptg_id=context.current['id'])
+                and self._is_ptg_provider(context, context.current['id'])):
+            context.delete_service_vm_sg = True
+        else:
+            context.delete_service_vm_sg = False
 
     @log.log
     def delete_policy_target_group_postcommit(self, context):
@@ -62,7 +107,7 @@ class OneConvergenceResourceMappingDriver(resource_mapping.ResourceMappingDriver
                                                  context.nsp_cleanup_ipaddress,
                                                  context.nsp_cleanup_fips)
         except Exception as e:
-            LOG.error(e)
+            LOG.exception((e))
         self._cleanup_redirect_action(context)
         # Cleanup SGs
         self._unset_sg_rules_for_subnets(
@@ -71,10 +116,174 @@ class OneConvergenceResourceMappingDriver(resource_mapping.ResourceMappingDriver
             context.current['consumed_policy_rule_sets'])
 
         l2p_id = context.current['l2_policy_id']
-        router_id = self._get_routerid_for_l2policy(context, l2p_id)
-        for subnet_id in context.current['subnets']:
-            self._cleanup_subnet(context._plugin_context, subnet_id, router_id)
+        try:
+            router_id = self._get_routerid_for_l2policy(context, l2p_id)
+            for subnet_id in context.current['subnets']:
+                self._cleanup_subnet(context._plugin_context, subnet_id, router_id)
+        except Exception as e:
+            LOG.exception((e))
         self._delete_default_security_group(
             context._plugin_context, context.current['id'],
             context.current['tenant_id'])
+        if context.delete_service_vm_sg:
+            self._delete_service_vm_sg(context, context.current['tenant_id'],
+                                       context.current['id'])
 
+    def _create_service_vm_sg_rule(self, context, sg_id):
+        attrs = {'tenant_id': context._plugin_context.tenant_id,
+                 'security_group_id': sg_id,
+                 'direction': 'ingress',
+                 'ethertype': 'IPv4',
+                 'protocol': None,
+                 'port_range_min': None,
+                 'port_range_max': None,
+                 'remote_ip_prefix': '0.0.0.0/0',
+                 'remote_group_id': None}
+
+        return self._create_sg_rule(context._plugin_context, attrs)
+
+    def _create_service_vm_sg(self, context, tenant_id, ptg_id):
+        port_name = 'svc_vm_%s' % ptg_id
+        attrs = {'name': port_name, 'tenant_id': tenant_id,
+                     'description': 'default'}
+        sg_id = self._create_sg(context._plugin_context, attrs)['id']
+        self._create_service_vm_sg_rule(context, sg_id)
+        return sg_id
+
+    def _get_service_vm_sg(self, plugin_context, tenant_id, ptg_id):
+        port_name = 'svc_vm_%s' % ptg_id
+        filters = {'name': [port_name]}
+        default_group = self._core_plugin.get_security_groups(
+            plugin_context, filters)
+        return default_group[0]['id'] if default_group else None
+
+    def _delete_service_vm_sg(self, context, tenant_id, ptg_id):
+        sg_id = self._get_service_vm_sg(context._plugin_context,
+                                        tenant_id, ptg_id)
+        if sg_id:
+            self._delete_sg(context._plugin_context, sg_id)
+
+    def _is_firewall_in_sc_spec(self, context, pt_id=None, ptg_id=None):
+        spec_id = self._get_redirect_action_spec_id(context, pt_id, ptg_id)
+        if spec_id:
+            sc_spec = self._servicechain_plugin.get_servicechain_spec(
+                                            context._plugin_context, spec_id)
+            for node_id in sc_spec['nodes']:
+                sc_node = self._servicechain_plugin.get_servicechain_node(
+                                            context._plugin_context, node_id)
+                profiles = self._servicechain_plugin.get_service_profiles(
+                                                    context._plugin_context)
+                for profile in profiles:
+                    if profile['service_type'] == pconst.FIREWALL:
+                        firewall_profile_id = profile['id']
+                if sc_node['service_profile_id'] == firewall_profile_id:
+                    return True
+        return False
+
+    def _get_policy_rule_set_ids(self, context, pt_id, ptg_id):
+        policy_rule_set_ids = []
+        if pt_id:
+            try:
+                pt = context._plugin.get_policy_target(context._plugin_context,
+                                                       pt_id)
+            except gp_ext.PolicyTargetNotFound:
+                LOG.warn(_("PT %s doesn't exist anymore"), pt_id)
+                return
+            ptg = context._plugin.get_policy_target_group(context._plugin_context,
+                                                pt['policy_target_group_id'])
+            if ptg['provided_policy_rule_sets']:
+                policy_rule_set_ids = ptg['provided_policy_rule_sets']
+        if ptg_id:
+            policy_rule_set_ids = context.current['provided_policy_rule_sets']
+        return policy_rule_set_ids
+
+    def _get_redirect_action_spec_id(self, context, pt_id, ptg_id):
+        policy_rule_set_ids = self._get_policy_rule_set_ids(context, pt_id, ptg_id)
+        if not policy_rule_set_ids:
+            return None
+
+        policy_rule_sets = context._plugin.get_policy_rule_sets(
+                                    context._plugin_context,
+                                    filters={'id': policy_rule_set_ids})
+        for policy_rule_set in policy_rule_sets:
+            policy_rules = context._plugin.get_policy_rules(
+                                context._plugin_context,
+                                filters={'id': policy_rule_set['policy_rules']})
+            for policy_rule in policy_rules:
+                policy_actions = context._plugin.get_policy_actions(
+                    context._plugin_context,
+                    filters={'id': policy_rule["policy_actions"],
+                             'action_type': [gconst.GP_ACTION_REDIRECT]})
+                for policy_action in policy_actions:
+                    if policy_action['action_type'] == gconst.GP_ACTION_REDIRECT:
+                        spec_id = policy_action.get("action_value")
+                        return spec_id
+
+    def _update_service_vm_rules(self, context, pt_id=None, ptg_id=None):
+        # Get all instances corresponding to a spec _id and update each instance.
+        spec_id = self._get_redirect_action_spec_id(context, pt_id, ptg_id)
+        if spec_id:
+            sc_instance_update_req = {
+                'servicechain_specs': [spec_id]}
+            sc_spec = self._servicechain_plugin.get_servicechain_specs(
+                context._plugin_context, filters={'id': [spec_id]})[0]
+            for sc_instance in sc_spec['instances']:
+
+                self._update_resource(
+                        self._servicechain_plugin,
+                        context._plugin_context,
+                        'servicechain_instance',
+                        sc_instance,
+                        sc_instance_update_req)
+
+    def _is_ptg_provider(self, context, ptg_id):
+        ptg = context._plugin.get_policy_target_group(context._plugin_context,
+                                                      ptg_id)
+        if ptg['provided_policy_rule_sets']:
+            return True
+        else:
+            return False
+
+
+    def _assoc_sgs_to_pt(self, context, pt_id, sg_list):
+        try:
+            pt = context._plugin.get_policy_target(context._plugin_context,
+                                                   pt_id)
+        except gp_ext.PolicyTargetNotFound:
+            LOG.warn(_("PT %s doesn't exist anymore"), pt_id)
+            return
+        port_id = pt['port_id']
+        port = self._core_plugin.get_port(context._plugin_context, port_id)
+        cur_sg_list = port[ext_sg.SECURITYGROUPS]
+        if (self._is_firewall_in_sc_spec(context, pt_id=pt_id) and
+            self._is_ptg_provider(context, pt['policy_target_group_id'])):
+            sg_id = self._get_service_vm_sg(context._plugin_context,
+                                    context._plugin_context.tenant_id,
+                                    pt['policy_target_group_id'])
+            new_sg_list = [sg_id]
+            # Commenting update part, until update changes upstreamed for plugin.
+            #self._update_service_vm_rules(context, pt_id=pt_id)
+        else:
+            new_sg_list = cur_sg_list + sg_list
+        port[ext_sg.SECURITYGROUPS] = new_sg_list
+        self._update_port(context._plugin_context, port_id, port)
+
+    def _disassoc_sgs_from_pt(self, context, pt_id, sg_list):
+        try:
+            pt = context._plugin.get_policy_target(context._plugin_context,
+                                                   pt_id)
+        except gp_ext.PolicyTargetNotFound:
+            LOG.warn(_("PT %s doesn't exist anymore"), pt_id)
+            return
+        port_id = pt['port_id']
+        self._disassoc_sgs_from_port(context._plugin_context, port_id, sg_list)
+
+    def _disassoc_sgs_from_port(self, plugin_context, port_id, sg_list):
+        try:
+            port = self._core_plugin.get_port(plugin_context, port_id)
+            cur_sg_list = port[ext_sg.SECURITYGROUPS]
+            new_sg_list = list(set(cur_sg_list) - set(sg_list))
+            port[ext_sg.SECURITYGROUPS] = new_sg_list
+            self._update_port(plugin_context, port_id, port)
+        except n_exc.PortNotFound:
+            LOG.warn(_("Port %s is missing") % port_id)
