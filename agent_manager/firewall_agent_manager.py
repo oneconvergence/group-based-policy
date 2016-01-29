8e422676a7cab2b0c17bb71045c20a043156e988
“AkashDeepSrivastava” <akash.deep@oneconvergence.com>
2016-01-29 17:41:39 +0530

    Removed Unwanted code

--- agent_manager/firewall_agent_manager.py
+++ agent_manager/firewall_agent_manager.py
@@ -26,8 +26,12 @@ from neutron.common.rpc import MessagingTimeout
 from neutron.db.firewall import firewall_db
 from neutron_fwaas.db.firewall import firewall_router_insertion_db
 from neutron.services.firewall import RestClientOverUnix as rest_client
+from neutron.common import exceptions
 
 LOG = logging.getLogger(__name__)
+
+class TenantNotFound(exceptions.NeutronException):
+    message = _("Cannot Get context as tenant_id is not available")
 
 rest_timeout = [
     cfg.IntOpt(
@@ -55,6 +59,9 @@ rest_timeout = [
         help=_("rest api timeout"))]
 cfg.CONF.register_opts(rest_timeout)
 
+REQUEST_METHOD = 'http' #can be http,https
+SERVER_IP_ADDR = '192.168.2.68'
+
 class OCFWAgent(manager.Manager, firewall_db.Firewall_db_mixin,
                 firewall_router_insertion_db.FirewallRouterInsertionDbMixin):
 
@@ -72,18 +79,6 @@ class OCFWAgent(manager.Manager, firewall_db.Firewall_db_mixin,
         self.state_rpc = agent_rpc.PluginReportStateAPI(
             local_constants.OC_FW_PLUGIN_TOPIC)
         self.report_interval = cfg.CONF.ocfwaas.oc_report_interval
-
-        if not self.oc_fwaas_enabled:
-            msg = "FWaaS not enabled in configuration file"
-            LOG.error(_(msg))
-            raise SystemExit(1)
-        else:
-            # self.driver = importutils.import_object(fwaas_driver_class_path)
-            self.load_driver()
-
-        # first handle all firewalls in PEDNING_DELETE state to avoid race with
-        # new firewall create requests which consumers will receive
-        self.sync_firewalls()
         self.endpoints = [FwAgent_SM_Callbacks(self)]
         self.conn = n_rpc.create_connection(new=True)
         self.conn.create_consumer(
@@ -98,23 +93,14 @@ class OCFWAgent(manager.Manager, firewall_db.Firewall_db_mixin,
             'host': cfg.CONF.host,
             'topic': local_constants.OC_FW_AGENT_TOPIC,
             'agent_type': local_constants.OC_AGENT_TYPE,
-            'configurations': {
-                'driver': local_constants.OC_FIREWALL_DRIVER
-            },
             'report_interval': self.report_interval,
             'description': 'oc firewall agent '
-
         }
 
         if self.report_interval:
             self.heartbeat = loopingcall.FixedIntervalLoopingCall(
                 self._report_state)
             self.heartbeat.start(interval=self.report_interval)
-
-    def load_driver(self):
-        for fw_driver in self.fwaas_drivers.split(","):
-            driver, vendor = fw_driver.split(":")
-            self.drivers[vendor] = importutils.import_object(driver)
 
     def after_start(self):
         LOG.debug(_(" OC FW agent started "))
@@ -136,70 +122,39 @@ class OCFWAgent(manager.Manager, firewall_db.Firewall_db_mixin,
         except Exception:
             LOG.exception(_("OC Firewall agent failed reporting state!"))
 
-    def get_firewalls_for_tenant(self, context):
-        """Agent uses this to get all firewalls and rules for a tenant."""
-        LOG.debug("get_firewalls_for_tenant() called")
-        fw_list = []
-        tmp_fw_list = super(OCFWAgent, self).\
-            get_firewalls(context)
-        for fw in tmp_fw_list:
-            fw_with_rules = super(OCFWAgent, self).\
-                _make_firewall_dict_with_rules(context, fw['id'])
-            if fw['status'] == n_const.PENDING_DELETE:
-                fw_with_rules['add-router-ids'] = []
-                fw_with_rules['del-router-ids'] = (
-                    super(OCFWAgent, self).get_firewall_routers(context, fw['id']))
-            else:
-                fw_with_rules['add-router-ids'] = (
-                    super(OCFWAgent, self)..get_firewall_routers(context, fw['id']))
-                fw_with_rules['del-router-ids'] = []
-            fw_list.append(fw_with_rules)
-        return fw_list
-
-    def sync_firewalls(self):
-        fw_list = []
-        try:
-            fw_list = self.get_firewalls_for_tenant(self.context)
-        except MessagingTimeout as e:
-            LOG.error(" Connection to firewall plugin failed with "
-                      " MessagingTimeout exception.Some stale fw service "
-                      " resources will not be cleaned." )
-            #raise SystemExit
-
-        for fw in fw_list:
-            if fw['status'] == 'PENDING_DELETE':
-                self.invoke_driver_for_plugin_api(self.context, fw,
-                                                  'delete_firewall')
-
     def create_firewall(self, context, firewall, host):
         LOG.debug(_("create firewall called"))
         LOG.debug(_("Firewall - %r" % firewall))
-        return self.invoke_driver_for_plugin_api(context, firewall,
+        return self.invoke_driver_for_plugin_api(context, firewall, host,
                                                  'create_firewall')
 
     def update_firewall(self, context, firewall, host):
         LOG.debug(_("update firewall called"))
         LOG.debug(_("Firewall - %r" % firewall))
-        return self.invoke_driver_for_plugin_api(context, firewall,
+        return self.invoke_driver_for_plugin_api(context, firewall, host,
                                                  'update_firewall')
 
     def delete_firewall(self, context, firewall, host):
         LOG.debug(_("delete firewall called"))
         LOG.debug(_("Firewall - %r" % firewall))
-        return self.invoke_driver_for_plugin_api(context, firewall,
+        return self.invoke_driver_for_plugin_api(context, firewall, host,
                                                  'delete_firewall')
 
-    def invoke_driver_for_plugin_api(self, context, fw, func_name):
+    def invoke_driver_for_plugin_api(self, context, fw, host, func_name):
         """
         :param context:
         :param fw:
+        :param host:
         :param func_name:
         :return:
         """
+        tenant_id = fw['tenant_id']
+        data_context = self._get_all_context_for_given_tenant(context, tenant_id)
+        kwargs = {'fw':fw, 'host':host}
+        body = {'kwargs': **kwargs,
+                'context': data_context}
+
         if func_name.lower() == 'create_firewall':
-            data_context = self._get_all_context_for_given_tenant(context)
-            body = {'resource_data': fw,
-                    'tenant_data': data_context}
             rest_client.send_request(REQUEST_METHOD,
                                      SERVER_IP_ADDR,
                                      'fw/create_firewall',
@@ -208,9 +163,6 @@ class OCFWAgent(manager.Manager, firewall_db.Firewall_db_mixin,
                                      body = body)
 
         elif func_name.lower() == 'delete_firewall':
-            data_context = self._get_all_context_for_given_tenant(context)
-            body = {'resource_data': fw,
-                    'tenant_data': data_context}
             rest_client.send_request(REQUEST_METHOD,
                                      SERVER_IP_ADDR,
                                      'fw/delete_firewall',
@@ -219,9 +171,6 @@ class OCFWAgent(manager.Manager, firewall_db.Firewall_db_mixin,
                                      body = body)
 
         elif func_name.lower() == 'update_firewall':
-            data_context = self._get_all_context_for_given_tenant(context)
-            body = {'resource_data': fw,
-                    'tenant_data': data_context}
             rest_client.send_request(REQUEST_METHOD,
                                      SERVER_IP_ADDR,
                                      'fw/update_firewall',
@@ -263,8 +212,8 @@ class OCFWAgent(manager.Manager, firewall_db.Firewall_db_mixin,
                 'routers':routers,
                 'ports':ports}
 
-    def _get_all_context_for_given_tenant(self, context, tenant_id=None):
-        if tenant_id == None and context.is_admin :
+    def _get_all_context_for_given_tenant(self, context, tenant_id):
+        if context.is_admin :
             tenant_id = context.tenant_id
         if tenant_id == None :
             raise TenantNotFound()
--- agent_manager/vpn_agent_manager.py
+++ agent_manager/vpn_agent_manager.py
@@ -7,7 +7,6 @@ from neutron.agent import rpc as agent_rpc
 from neutron.common import exceptions
 from neutron.services.vpn.common import topics
 from neutron import context
-from neutron.openstack.common import lockutils
 from neutron.openstack.common import importutils
 from neutron.openstack.common import log as logging
 from neutron.openstack.common import loopingcall
@@ -15,15 +14,6 @@ from neutron.openstack.common import periodic_task
 from neutron.plugins.common import constants
 from neutron.services.vpn.common import constants as vpn_const
 from neutron.services.vpn import RestClientOverUnix as rest_client
-
-class UnknownSvcTypeException(exceptions.NeutronException):
-    message = _("Unsupported rpcsvc_type '%(svc_type)s' from plugin ")
-
-class UnknownResourceException(exceptions.NeutronException):
-    message = _("Unsupported resource '%(resource)s' from plugin ")
-
-class UnknownReasonException(exceptions.NeutronException):
-    message = _("Unsupported rpcreason '%(reason)s' from plugin ")
 
 class TenantNotFound(exceptions.NeutronException):
     message = _("Cannot Get context as tenant_id is not available")
@@ -63,22 +53,6 @@ class VpnAgentManager(vpn_db.VPNPluginDb, vpn_db.VPNPluginRpcDbMixin):
             heartbeat = loopingcall.FixedIntervalLoopingCall(
                 self._report_state)
             heartbeat.start(interval=report_interval)
-        self.handlers = {
-            vpn_const.SERVICE_TYPE_IPSEC :{
-                'vpn_service': {
-                    'create': self.create_vpn_service},
-                'ipsec_site_connection': {
-                    'create': self.create_ipsec_conn,
-                    'update': self.update_ipsec_conn,
-                    'delete': self.delete_ipsec_conn}},
-            vpn_const.SERVICE_TYPE_OPENVPN : {
-                'vpn_service': {
-                    'create': self.create_vpn_service},
-                'ssl_vpn_connection': {
-                    'create': self.create_sslvpn_conn,
-                    'update': self.update_sslvpn_conn,
-                    'delete': self.delete_sslvpn_conn}}
-            }
         self._core_plugin = None
 
     def _report_state(self):
@@ -99,315 +73,13 @@ class VpnAgentManager(vpn_db.VPNPluginDb, vpn_db.VPNPluginRpcDbMixin):
     def vpnservice_updated(self, context, **kwargs):
         LOG.debug(_("vpnservice_updated from server side"))
         resource = kwargs.get('resource')
-        tenant_id = resource['tenant_id']
-        @lockutils.synchronized(tenant_id)
-        def _vpnservice_updated(context, **kwargs):
-            svc_type = kwargs.get('svc_type')
-            rsrc = kwargs.get('rsrc_type')
-            reason = kwargs.get('reason')
-            if svc_type not in self.handlers.keys():
-                raise UnknownSvcTypeException(svc_type=svc_type)
-            if rsrc not in self.handlers[svc_type].keys():
-                raise UnknownResourceException(resource=driver)
-            if reason not in self.handlers[svc_type][rsrc].keys():
-                raise UnknownReasonException(reason=reason)
-
-            self.handlers[svc_type][rsrc][reason](context,  **kwargs)
-        return _vpnservice_updated(context, **kwargs)
-    '''
-    def _get_vpn_services(self, context, ids=None, filters=None):
-        vpnservices = []
-        if ids:
-            for id in ids:
-                vpnservice = super(VpnAgentManager, self).get_vpnservice(context,  id)
-                vpnservices.append(vpnservice)
-        else:
-            if filters:
-                vpnservices = super(VpnAgentManager, self).get_vpnservices(
-                    context, filters=filters)
-
-        return vpnservices
-    '''
-    def create_vpn_service(self, context, **kwargs):
-        svc = kwargs.get('resource')
-        filters = {'tenant_id': [context.tenant_id]}
-        #t_vpnsvcs = self._get_vpn_services(
-        #    context, filters=filters)
-        data_context = self._get_all_context_for_given_tenant(context)
-        body = {'resource_data': svc,
-                'tenant_data': data_context}
+        data_context = self._get_all_context_for_given_tenant(context, resource['tenant_id'])
+        body = {'kwargs': kwargs,
+                'context': data_context}
         rest_client.send_request(REQUEST_METHOD,
                                  SERVER_IP_ADDR,
-                                 'vpn/create_vpnservices',
+                                 'vpn/vpnservices_updated',
                                  'POST',
-                                 headers = 'application/json',
-                                 body = body) 
-    '''
-    def _get_ipsec_conns(self, context, filters):
-        ipsec_site_conns = super(VpnAgentManager, self).\
-            get_ipsec_site_connections(
-                context,
-                filters=filters,
-                fields=None)
-        return ipsec_site_conns
-    '''
-    '''
-    def _ipsec_get_tenant_conns(self, context, conn, on_delete=False):
-        filters = {
-            'tenant_id': [context.tenant_id],
-            'peer_address': [conn['peer_address']]}
-        tenant_conns = self._get_ipsec_conns(
-            context, filters)
-        if (not on_delete) and (tenant_conns):
-            tenant_conns.remove(conn)
-            copy_conns = copy.deepcopy(tenant_conns)
-            for tconn in copy_conns:
-                if tconn['status'] == vpn_const.STATE_PENDING:
-                    tenant_conns.remove(tconn)
-        return tenant_conns
-    '''
-    '''
-    def _make_vpnservice_context(self, vpnservices):
-        return vpnservices.values()
-    '''
-    '''
-    def _get_ipsec_site2site_contexts(self, context,  filters=None):
-        vpnservices = {}
-        core_plugin = self.core_plugin
-        s_filters = {}
-        if 'tenant_id' in filters:
-            s_filters['tenant_id'] = [filters['tenant_id']]
-        if 'vpnservice_id' in filters:
-            s_filters['vpnservice_id'] = [filters['vpnservice_id']]
-        if 'siteconn_id' in filters:
-            s_filters['id'] = [filters['siteconn_id']]
-        if 'peer_address' in filters:
-            s_filters['peer_address'] = [filters['peer_address']]
-
-        ipsec_site_conns = super(VpnAgentManager, self).get_ipsec_site_connections(
-            context,
-            filters=s_filters,
-            fields=None)
-
-        for ipsec_site_conn in ipsec_site_conns:
-            vpnservice = super(VpnAgentManager, self).get_vpnservice(
-                context,
-                ipsec_site_conn['vpnservice_id'])
-
-            ikepolicy = super(VpnAgentManager, self).get_ikepolicy(
-                context,
-                ipsec_site_conn['ikepolicy_id'])
-
-            ipsecpolicy = super(VpnAgentManager, self).get_ipsecpolicy(
-                context,
-                ipsec_site_conn['ipsecpolicy_id'])
-            cidr = core_plugin.get_subnet(
-                context,
-                vpnservice['subnet_id'])['cidr']
-            vpnservice['cidr'] = cidr
-
-            siteconn = {}
-            siteconn['connection'] = ipsec_site_conn
-            siteconn['ikepolicy'] = ikepolicy
-            siteconn['ipsecpolicy'] = ipsecpolicy
-            vpnserviceid = vpnservice['id']
-
-            if vpnserviceid not in vpnservices.keys():
-                vpnservices[vpnserviceid] = \
-                    {'service': vpnservice, 'siteconns': []}
-
-            vpnservices[vpnserviceid]['siteconns'].append(siteconn)
-
-        site2site_context = self._make_vpnservice_context(vpnservices)
-        return site2site_context
-    '''
-    '''
-    def _get_ssl_vpn_contexts(self, context, filters=None):
-        vpnservices = {}
-
-        core_plugin = self.core_plugin
-        s_filters = {}
-
-        if 'tenant_id' in filters:
-                    s_filters['tenant_id'] = [filters['tenant_id']]
-        if 'vpnservice_id' in filters:
-                    s_filters['vpnservice_id'] = [filters['vpnservice_id']]
-        if 'sslvpnconn_id' in filters:
-                    s_filters['id'] = [filters['sslvpnconn_id']]
-
-        ssl_vpn_conns = super(VpnAgentManager, self).get_ssl_vpn_connections(
-            context,
-            filters=s_filters,
-            fields=None)
-
-        for ssl_vpn_conn in ssl_vpn_conns:
-            vpnservice = super(VpnAgentManager, self).get_vpnservice(
-                context,
-                ssl_vpn_conn['vpnservice_id'])
-
-            cidr = core_plugin.get_subnet(
-                context,
-                vpnservice['subnet_id'])['cidr']
-
-            vpnservice['cidr'] = cidr
-
-            sslconn = {}
-            sslconn['connection'] = ssl_vpn_conn
-            sslconn['credential'] = None
-            vpnserviceid = vpnservice['id']
-
-            if vpnserviceid not in vpnservices.keys():
-                vpnservices[vpnserviceid] = \
-                    {'service': vpnservice, 'sslvpnconns': []}
-            vpnservices[vpnserviceid]['sslvpnconns'].append(sslconn)
-
-        sslvpn_context = self._make_vpnservice_context(vpnservices)
-        return sslvpn_context
-    '''
-    '''
-    def _get_vpn_servicecontext(self,  context,  svctype,  filters=None):
-        if svctype == vpn_const.SERVICE_TYPE_IPSEC:
-            return self._get_ipsec_site2site_contexts(context,  filters)
-        elif svctype == vpn_const.SERVICE_TYPE_OPENVPN:
-            return self._get_ssl_vpn_contexts(context, filters)
-    '''
-    '''
-    def _get_ipsec_contexts(self, context, tenant_id=None,
-                           vpnservice_id=None, conn_id=None,
-                           peer_address=None):
-        filters = {}
-        if tenant_id:
-            filters['tenant_id'] = tenant_id
-        if vpnservice_id:
-            filters['vpnservice_id'] = vpnservice_id
-        if conn_id:
-            filters['siteconn_id'] = conn_id
-        if peer_address:
-            filters['peer_address'] = peer_address
-
-        return self.\
-            _get_vpn_servicecontext(
-                context,
-                vpn_const.SERVICE_TYPE_IPSEC, filters)
-    '''
-    def create_ipsec_conn(self, context, **kwargs):
-        conn = kwargs.get('resource')
-        #vpnservice_id = conn['vpnservice_id']
-        #tenant_conns = self._ipsec_get_tenant_conns(
-        #    context, conn)
-        #svc_contexts = self._get_ipsec_contexts(
-        #    context, conn_id=conn['id'])
-        data_context = self._get_all_context_for_given_tenant(context)
-        body = {'resource_data': conn,
-                'tenant_data': data_context}
-        rest_client.send_request(REQUEST_METHOD,
-                                 SERVER_IP_ADDR,
-                                 'vpn/create_ipsec_conn',
-                                 'POST',
-                                 headers = 'application/json',
-                                 body = body) 
-
-    def update_ipsec_conn(self, context, **kwargs):
-        update_conn = kwargs.get('resource')
-        #svc_contexts = self._get_ipsec_contexts(
-        #    context, conn_id=kwargs.get('id'))
-        data_context = self._get_all_context_for_given_tenant(context)
-        body = {'resource_data': update_conn,
-                'tenant_data': data_context}
-        rest_client.send_request(REQUEST_METHOD,
-                                 SERVER_IP_ADDR,
-                                 'vpn/update_ipsec_conn',
-                                 'PUT',
-                                 headers = 'application/json',
-                                 body = body) 
-
-    def delete_ipsec_conn(self, context, **kwargs):
-        conn = kwargs.get('resource')
-        #vpn_svcs = self._get_vpn_services(
-        #    context, ids=[conn['vpnservice_id']])
-        #tenant_conns = self._ipsec_get_tenant_conns(
-        #    context, conn, on_delete=True)
-        data_context = self._get_all_context_for_given_tenant(context)
-        body = {'resource_data': update_conn,
-                'tenant_data': data_context}
-        rest_client.send_request(REQUEST_METHOD,
-                                 SERVER_IP_ADDR,
-                                 'vpn/delete_ipsec_conn',
-                                 'DELETE',
-                                 headers = 'application/json',
-                                 body = body)
-    '''
-    def _get_ssl_vpn_conns(self, context, filters):
-        ssl_vpn_conns = super(VpnAgentManager, self).\
-            get_ssl_vpn_connections(
-                context,
-                filters=filters)
-        return ssl_vpn_conns
-    '''
-    '''
-    def _sslvpn_get_tenant_conns(self, context, conn, on_delete=False):
-        filters = {'tenant_id': [context.tenant_id]}
-        tenant_conns = self._get_ssl_vpn_conns(
-            context, filters=filters)
-
-        if not on_delete:
-            tenant_conns.remove(conn)
-            copy_conns = copy.deepcopy(tenant_conns)
-            for tconn in copy_conns:
-                if tconn['status'] == vpn_const.STATE_PENDING:
-                    tenant_conns.remove(tconn)
-        return tenant_conns
-    '''
-    '''
-    def _get_sslvpn_contexts(self, context, tenant_id=None,
-                            vpnservice_id=None, conn_id=None):
-        filters = {}
-        if tenant_id:
-            filters['tenant_id'] = tenant_id
-        if vpnservice_id:
-            filters['vpnservice_id'] = vpnservice_id
-        if conn_id:
-            filters['sslvpnconn_id'] = conn_id
-
-        return self.\
-            _get_vpn_servicecontext(
-                context,
-                vpn_const.SERVICE_TYPE_OPENVPN, filters)
-    '''
-    def create_sslvpn_conn(self, context, **kwargs):
-        conn = kwargs.get('resource')
-        #t_conns = self._sslvpn_get_tenant_conns(
-        #    context, conn)
-        #svc_context = self._get_sslvpn_contexts(
-        #    context, conn_id=conn['id'])
-
-        data_context = self._get_all_context_for_given_tenant(context)
-        body = {'resource_data': conn,
-                'tenant_data': data_context}
-        rest_client.send_request(REQUEST_METHOD,
-                                 SERVER_IP_ADDR,
-                                 'vpn/create_sslvpn_conn',
-                                 'POST',
-                                 headers = 'application/json',
-                                 body = body)
-
-    def update_sslvpn_conn(self, context, **kwargs):
-        pass
-
-    def delete_sslvpn_conn(self, context, **kwargs):
-        conn = kwargs.get('resource')
-        #vpn_svcs = self._get_vpn_services(
-        #    context, [conn['vpnservice_id']])
-        #t_conns = self._sslvpn_get_tenant_conns(
-        #    context, conn, on_delete=True)
-
-        data_context = self._get_all_context_for_given_tenant(context)
-        body = {'resource_data': conn,
-                'tenant_data': data_context}
-        rest_client.send_request(REQUEST_METHOD,
-                                 SERVER_IP_ADDR,
-                                 'vpn/delete_sslvpn_conn',
-                                 'DELETE',
                                  headers = 'application/json',
                                  body = body)
 
@@ -451,11 +123,11 @@ class VpnAgentManager(vpn_db.VPNPluginDb, vpn_db.VPNPluginRpcDbMixin):
                 'routers':routers}
 
 
-    def _get_all_context_for_given_tenant(self, context, tenant_id=None):
-        if tenant_id == None and context.is_admin :
+    def _get_all_context_for_given_tenant(self, context, tenant_id):
+        if context.is_admin :
             tenant_id = context.tenant_id
         if tenant_id == None :
-            raise TenantNotFound() 
+            raise TenantNotFound()
         filters = {'tenant_id':tenant_id}
         data_context = self._get_vpn_context(context, filters)
         data_context.update(self._get_core_context(context, filters))
