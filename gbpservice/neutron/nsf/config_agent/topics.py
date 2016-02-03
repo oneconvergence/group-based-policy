402d593b5da249b333cb6fab87ac1460eabed788
“AkashDeepSrivastava” <akash.deep@oneconvergence.com>
2016-02-03 20:06:30 +0530

    Ran autopep8 on config_agent folder and module folder

--- gbpservice/neutron/nsf/config_agent/topics.py
+++ gbpservice/neutron/nsf/config_agent/topics.py
@@ -1,4 +1,4 @@
-FW_NSF_CONFIGAGENT_TOPIC='fw_nsf_configagent_topic'
+FW_NSF_CONFIGAGENT_TOPIC = 'fw_nsf_configagent_topic'
 LB_NSF_CONFIGAGENT_TOPIC = 'lb_nsf_configagent_topic'
 VPN_NSF_CONFIGAGENT_TOPIC = 'vpn_nsf_configagent_topic'
 SM_NSF_CONFIGAGENT_TOPIC = 'sm_nsf_configagent_topic'
