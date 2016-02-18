9117be176165f31d8a827f825c0b45d5618399fc
pkharat <pritam.kharat@oneconvergence.com>
2016-02-18 16:40:53 +0530

    1) Removing agents directory, load_drivers.py 2) Added utils.py which has load_drivers() and load_agents()

--- gbpservice/neutron/nsf/configurator/lib/demuxer.py
+++ gbpservice/neutron/nsf/configurator/lib/demuxer.py
@@ -3,24 +3,37 @@ class ConfiguratorDemuxer(object):
     def __init__(self):
         pass
 
-    def get_service_agent_info(self, method, data):
-        sa_info = {}
+    def get_service_agent_info(self, operation, request_data):
+        sa_info_list = []
 
-        if 'service_type' in data['info']:
-            sa_info['service_type'] = data['info']['service_type']
-        else:
-            sa_info['service_type'] = None
+        service_type = request_data['info'].get('type')
+        for config_data in request_data['config']:
+            sa_info = {}
+            if service_type:
+                sa_info.update({'service_type': service_type})
 
-        if data['info']['service_type'] == 'firewall':
-            sa_info['method'] = method + data['config']['resource']
-        elif data['info']['service_type'] == 'vpn':
-            sa_info['method'] = 'vpn_service_updated'
-        elif data['info']['service_type'] == 'loadbalancer':
-            sa_info['method'] = method + data['config']['resource']
-        else:
-            if method == 'create':
-                sa_info['method'] = 'configure' + data['config']['resource']
-            elif method == 'delete':
-                sa_info['method'] = 'clear' + data['config']['resource']
-            elif method == 'update':
-                sa_info['method'] = 'update' + data['config']['resource']
+                if service_type == 'firewall':
+                    method = operation + '_' + config_data['resource']
+                elif service_type == 'vpn':
+                    method = 'vpn_service_updated'
+                elif service_type == 'loadbalancer':
+                    method = operation + '_' + config_data['resource']
+            else:
+                sa_info.update({'service_type': 'generic_config'})
+                if operation == 'create':
+                    method = 'configure_' + config_data['resource']
+                elif operation == 'clear':
+                    method = 'clear_' + config_data['resource']
+            sa_info.update({'method': method})
+
+            ''' [DEE]: We need a mapping between the data and the agent
+                       information present in a list. We can achieve this
+                       either by mapping data in the sa_info or by popping
+                       the list as list is ordered.
+            '''
+            data = config_data['kwargs']
+            sa_info.update({'kwargs': data})
+
+            sa_info_list.append(sa_info)
+
+        return sa_info_list
--- gbpservice/neutron/nsf/modules/configurator.py
+++ gbpservice/neutron/nsf/modules/configurator.py
@@ -1,12 +1,12 @@
-import sys
-import os
-import oslo_messaging as messaging
 from oslo_log import log
+
 from gbpservice.neutron.nsf.core.main import RpcAgent
 from gbpservice.neutron.nsf.configurator.lib import topics
 from gbpservice.neutron.nsf.configurator.lib.demuxer import ConfiguratorDemuxer
+from gbpservice.neutron.nsf.configurator.lib.utils import Utils
 
 LOG = log.getLogger(__name__)
+AGENTS_PKG = 'gbpservice.neutron.nsf.configurator.agents'
 
 
 class ConfiguratorRpcManager(object):
@@ -17,27 +17,34 @@ class ConfiguratorRpcManager(object):
         self.demuxer = demuxer
 
     def _get_service_agent_obj(self, service_type):
-        return self.service_agents[service_type]
+        return self.sa.service_agent_objs[service_type]
 
     def _invoke_service_agent_method(self, method, request_data):
-
         try:
-            sa_info = self.demuxer.get_service_agent_info(method, request_data)
-        except Exception as e:
-            raise e
+            sa_info_list = self.demuxer.get_service_agent_info(method,
+                                                               request_data)
+        except Exception as err:
+            msg = ("Failed to demultiplex RPC requests in Configurator. " +
+                   str(err).capitalize())
+            LOG.error(msg)
+            raise msg
 
-        try:
-            sa_obj = self._get_service_agent_obj(sa_info['service_type'])
-        except Exception as e:
-            raise e
+        for sa_info in sa_info_list:
+            try:
+                sa_obj = self._get_service_agent_obj(sa_info['service_type'])
+            except Exception as err:
+                msg = ("Failed to get service agent object in Configurator. " +
+                       str(err).capitalize())
+                LOG.error(msg)
+                raise msg
 
-        '''TODO:(pritam) fetch context from request_data
-        '''
-        context = ''
-        try:
-            sa_obj.sa_info['method'](context, request_data['config'])
-        except Exception as e:
-            raise e
+            try:
+                getattr(sa_obj, sa_info['method'])(**sa_info['kwargs'])
+            except Exception as err:
+                msg = ("Failed to call service agent RPC manager from "
+                       "Configurator. " + str(err).capitalize())
+                LOG.error(msg)
+                raise msg
 
     def create_network_device_config(self, context, request_data):
         self._invoke_service_agent_method('create', request_data)
@@ -56,28 +63,6 @@ class ConfiguratorModule(object):
     def __init__(self):
         self.service_agent_objs = {}
         self.imported_service_agents = []
-
-    def import_service_agents(self):
-        pkg = 'gbpservice.neutron.nsf.configurator.agents'
-        base_agent = __import__(pkg,
-                                globals(), locals(), ['agents'], -1)
-        agents_dir = base_agent.__path__[0]
-        syspath = sys.path
-        sys.path = [agents_dir] + syspath
-        try:
-            files = os.listdir(agents_dir)
-        except OSError:
-            print "Failed to read files"
-            files = []
-
-        for fname in files:
-            if fname.endswith(".py") and fname != '__init__.py':
-                agent = __import__(pkg, globals(),
-                                   locals(), [fname[:-3]], -1)
-                self.imported_service_agents += [
-                                eval('agent.%s' % (fname[:-3]))]
-                # modules += [__import__(fname[:-3])]
-        sys.path = syspath
 
     def register_service_agent(self, service_type, service_agent):
         if service_type not in self.service_agent_objs:
@@ -115,23 +100,39 @@ class ConfiguratorModule(object):
 def init_rpc(sc, cm, conf, demuxer):
     rpc_mgr = ConfiguratorRpcManager(sc, cm, conf, demuxer)
     configurator_agent = RpcAgent(sc,
-                                  topics.CONFIGURATOR,
-                                  rpc_mgr)
+                                  topic=topics.CONFIGURATOR,
+                                  manager=rpc_mgr)
 
     sc.register_rpc_agents([configurator_agent])
 
 
 def get_configurator_module_handle():
     cm = ConfiguratorModule()
-    cm.import_service_agents()
+    cm.imported_service_agents = Utils().load_agents(AGENTS_PKG)
+    LOG.info(" Imported agents %s " % (cm.imported_service_agents))
     return cm
 
 
 def module_init(sc, conf):
-    cm = get_configurator_module_handle()
-    demuxer = ConfiguratorDemuxer()
-    cm.init_service_agents(sc, conf)
-    init_rpc(sc, cm, conf, demuxer)
+    try:
+        cm = get_configurator_module_handle()
+        demuxer = ConfiguratorDemuxer()
+    except Exception as err:
+        LOG.error("Configurator Demuxer initialization unsuccessful. %s"
+                  % (str(err).capitalize()))
+        raise err
+    else:
+        LOG.debug("Configurator Demuxer initialization successful.")
+
+    try:
+        cm.init_service_agents(sc, conf)
+        init_rpc(sc, cm, conf, demuxer)
+    except Exception as err:
+        LOG.error("Configurator RPC initialization unsuccessful. %s"
+                  % (str(err).capitalize()))
+        raise err
+    else:
+        LOG.debug("Configurator RPC initialization successful.")
 
 
 def init_complete(sc, conf):
