23d019f3257c3ba32a8eb9442d93c498e4ddf49d
mak <ahmed.khan@oneconvergence.com>
2016-02-23 15:06:00 +0530

    Fixed all the errors reported by tox env of gbp repo

--- gbpservice/nfp/core/fifo.py
+++ gbpservice/nfp/core/fifo.py
@@ -10,11 +10,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import collections
 import Queue
 import sys
-from collections import deque
 
-""" Implements FIFO using python deque.
+deque = collections.deque
+
+"""Implements FIFO using python deque.
 
     New methods to support 'get' more than one element,
     'copy' the queue, 'remove' multiple messages are added.
@@ -25,16 +27,14 @@ class Fifo(object):
 
     class Empty(Exception):
 
-        '''
-        Exception raised when queue is empty and dequeue is attempted.
-        '''
+        """Exception raised when queue is empty and dequeue is attempted.
+        """
         pass
 
     class Full(Exception):
 
-        '''
-        Exception raised when queue is full and enqueue is attempted.
-        '''
+        """Exception raised when queue is full and enqueue is attempted.
+        """
         pass
 
     def __init__(self, sc, maxsize=-1):
@@ -58,7 +58,7 @@ class Fifo(object):
         return out
 
     def put(self, msg):
-        """ Puts a message in queue. """
+        """Puts a message in queue. """
         try:
             self._sc.lock()
             self._is_full()
@@ -67,7 +67,7 @@ class Fifo(object):
             self._sc.unlock()
 
     def get(self, limit=sys.maxint):
-        """ Get passed number of messages.
+        """Get passed number of messages.
 
             If there are less messages in the queue than requested,
             then available number of messages are returned.
@@ -85,14 +85,14 @@ class Fifo(object):
             return msgs
 
     def copy(self):
-        """ Copies the queue and returns the copy. """
+        """Copies the queue and returns the copy. """
         self._sc.lock()
         qu = list(self._q)
         self._sc.unlock()
         return qu
 
     def remove(self, msgs):
-        """ Remove list of messages from the fifo """
+        """Remove list of messages from the fifo """
         try:
             self._sc.lock()
             for msg in msgs:
