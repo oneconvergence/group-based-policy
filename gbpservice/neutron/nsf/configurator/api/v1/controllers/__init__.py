6970d59e3d0347e9abb9470a8a2578e4a9a97f3a
hiteshhapani <hitesh.hapani@oneconvergence.com>
2016-02-26 18:36:22 +0530

    change in name of controller handler

--- gbpservice/neutron/nsf/configurator/api/v1/controllers/__init__.py
+++ gbpservice/neutron/nsf/configurator/api/v1/controllers/__init__.py
@@ -9,11 +9,11 @@ import controller
 class ControllerResolver(object):
     """this class send parameter to controller class
     according to query string"""
-    create_network_device_config = controller.Controller(
+    create_network_function_device_config = controller.Controller(
         "create_network_device_config")
-    delete_network_device_config = controller.Controller(
+    delete_network_function_device_config = controller.Controller(
         "delete_network_device_config")
-    update_network_device_config = controller.Controller(
+    update_network_function_device_config = controller.Controller(
         "update_network_device_config")
     create_network_function_config = controller.Controller(
         "create_network_service_config")
