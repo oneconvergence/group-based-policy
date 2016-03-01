2aba110fa46e53a3d2ba9441e52c65b7819b423b
pkharat <pritam.kharat@oneconvergence.com>
2016-03-01 15:18:03 +0530

    Added HM apis in the BaseDriver class

--- gbpservice/neutron/nsf/configurator/drivers/base/base_driver.py
+++ gbpservice/neutron/nsf/configurator/drivers/base/base_driver.py
@@ -1,6 +1,8 @@
+import subprocess
 from oslo_log import log as logging
 LOG = logging.getLogger(__name__)
 SUCCESS = 'SUCCESS'
+FAILED = 'FAILED'
 
 
 class BaseDriver(object):
@@ -21,4 +23,22 @@ class BaseDriver(object):
         return SUCCESS
 
     def clear_routes(self, context, kwargs):
+        return SUCCESS
+
+    def configure_healthmonitor(self, context, kwargs):
+        # Ping based basic HM support provided by BaseDriver.
+        # Service provider can override the method implementation
+        # if they want to support other types.
+        ip = kwargs.get('mgmt_ip')
+        COMMAND = 'ping -c5 '+ip
+        try:
+            subprocess.check_output(COMMAND, stderr=subprocess.STDOUT,
+                                    shell=True)
+        except Exception:
+            # LOG.error("Health check failed for vm=%s, ip=%s," % (
+            #                                        kwargs.get('vmid'), ip))
+            return FAILED
+        return SUCCESS
+
+    def clear_healthmonitor(self, context, kwargs):
         return SUCCESS
