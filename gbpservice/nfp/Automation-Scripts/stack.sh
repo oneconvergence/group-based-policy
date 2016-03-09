9e12514ad751e1a33b7df0368730877c64b3ddb3
“Dhuldev-Valekar” <dhuldev.valekar@oneconvergence.com>
2016-03-09 18:26:11 +0530

    Added SC-GBP-SCRIPTS

--- gbpservice/nfp/Automation-Scripts/stack.sh
+++ gbpservice/nfp/Automation-Scripts/stack.sh
@@ -1393,6 +1393,7 @@ fi
 # Indicate how long this took to run (bash maintained variable ``SECONDS``)
 echo_summary "stack.sh completed in $SECONDS seconds."
 GBP_SCRIPT_DIR=/opt/stack/gbp/gbpservice/nfp/Automation-Scripts/GBP_SCRIPT
+sudo cp -r /opt/stack/gbp/gbpservice/nfp/Automation-Scripts/SC-GBP-SCRIPTS /home/stack/.
 sudo bash $GBP_SCRIPT_DIR/configure_oc_gbp_params.sh $GBP_SCRIPT_DIR $TOP_DIR $EXT_NET_NAME $EXT_NET_SUBNET_NAME $EXT_NET_GATEWAY $EXT_NET_ALLOCATION_POOL_START $EXT_NET_ALLOCATION_POOL_END $EXT_NET_CIDR $EXT_NET_MASK 
 
 sudo bash $GBP_SCRIPT_DIR/wakeup_service.sh $ConfiguratorQcow2Image $VyosQcow2Image $GBP_SCRIPT_DIR $TOP_DIR
