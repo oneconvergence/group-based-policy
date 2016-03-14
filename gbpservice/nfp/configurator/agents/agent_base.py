6eb635dc5caa6041bb80b74745ef01fbd4c8e404
Deepak <deepak.santhos@oneconvergence.com>
2016-03-14 15:16:36 +0530

    added firewall directory structure

--- gbpservice/nfp/configurator/agents/agent_base.py
+++ gbpservice/nfp/configurator/agents/agent_base.py
@@ -1,6 +1,18 @@
-from oslo_log import log as logging
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from gbpservice.nfp.configurator.lib import (
     generic_config_constants as const)
+from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
 
@@ -15,24 +27,36 @@ module invokes these methods through the service agent's child class instance.
 class AgentBaseRPCManager(object):
 
     def __init__(self, sc, conf):
-        self._sc = sc
+        self.sc = sc
         self.conf = conf
 
-    def validate_request(self, sa_info_list, notification_data):
-        if (isinstance(sa_info_list, list) and
+    def validate_request(self, sa_req_list, notification_data):
+        """Preliminary validation of function input.
+
+        :param sa_req_list: List of data blobs prepared by de-multiplexer
+        for service agents processing.
+        :param notification_data: Notification blobs prepared by the service
+        agents after processing requests blobs. Each request blob will have
+        a corresponding notification blob.
+
+        Returns: True if validation passes. False if validation fails.
+
+        """
+
+        if (isinstance(sa_req_list, list) and
                 isinstance(notification_data, dict)):
             return True
         else:
             return False
 
-    def forward_request(self, sa_info_list, notification_data):
+    def process_request(self, sa_req_list, notification_data):
         """Forwards the RPC message from configurator to service agents.
 
         Checks if the request message contains multiple data blobs. If multiple
         data blobs are found, a batch event is generated otherwise a single
         event.
 
-        :param sa_info_list: List of data blobs prepared by de-multiplexer
+        :param sa_req_list: List of data blobs prepared by de-multiplexer
         for service agents processing.
         :param notification_data: Notification blobs prepared by the service
         agents after processing requests blobs. Each request blob will have
@@ -43,41 +67,58 @@ class AgentBaseRPCManager(object):
         """
 
         # In case of malformed input, send failure notification
-        if not self.validate_request(sa_info_list, notification_data):
+        if not self.validate_request(sa_req_list, notification_data):
             # TODO: Need to send failure notification
             return
 
         # Multiple request data blobs needs batch processing. Send batch
         # processing event or do direct processing of single request data blob
-        if (len(sa_info_list) > 1):
+        if (len(sa_req_list) > 1):
             args_dict = {
-                'sa_info_list': sa_info_list,
+                'sa_req_list': sa_req_list,
                 'notification_data': notification_data
             }
-            ev = self._sc.new_event(id='PROCESS_BATCH', data=args_dict)
-            self._sc.post_event(ev)
+            ev = self.sc.new_event(id='PROCESS_BATCH', data=args_dict)
+            self.sc.post_event(ev)
         else:
-            sa_info_list[0]['context'].update(
+            sa_req_list[0]['context'].update(
                 {'notification_data': notification_data})
-            sa_info_list[0]['context'].update(
-                {'resource': sa_info_list[0]['resource']})
-            getattr(self, sa_info_list[0]['method'])(
-                sa_info_list[0]['context'],
-                **sa_info_list[0]['kwargs'])
+            sa_req_list[0]['context'].update(
+                {'resource': sa_req_list[0]['resource']})
+            getattr(self, sa_req_list[0]['method'])(
+                sa_req_list[0]['context'],
+                **sa_req_list[0]['kwargs'])
+
+
+class AgentBaseNotification(object):
+
+    def __init__(self, sc):
+        self.sc = sc
+
+    def _notification(self, data):
+        """Enqueues notification event into notification queue.
+
+        These events are enqueued into notification queue and are retrieved
+        when get_notifications() API lands on configurator.
+
+        :param data: Event data blob
+
+        Returns: None
+
+        """
+
+        event = self.sc.new_event(
+            id='NOTIFICATION_EVENT', key='NOTIFICATION_EVENT', data=data)
+        self.sc.poll_event(event)
 
 
 class AgentBaseEventHandler(object):
 
-    def __init__(self, sc, drivers, rpcmgr, nqueue):
-        self._sc = sc
+    def __init__(self, sc, drivers, rpcmgr):
+        self.sc = sc
         self.drivers = drivers
-        self._rpcmgr = rpcmgr
-        self.nqueue = nqueue
-
-    def _notification(self, data):
-        event = self._sc.new_event(
-            id='NOTIFICATION_EVENT', key='NOTIFICATION_EVENT', data=data)
-        self._sc.poll_event(event)
+        self.rpcmgr = rpcmgr
+        self.notify = AgentBaseNotification(self.sc)
 
     def process_batch(self, ev):
         """Processes a request with multiple data blobs.
@@ -96,17 +137,17 @@ class AgentBaseEventHandler(object):
         try:
             # Get service agent information list and notification data list
             # from the event data
-            sa_info_list = ev.data.get('sa_info_list')
+            sa_req_list = ev.data.get('sa_req_list')
             notification_data = ev.data.get('notification_data')
 
             # Process the first data blob from the service information list.
             # Get necessary parameters needed for driver method invocation.
-            method = sa_info_list[0]['method']
-            resource = sa_info_list[0]['resource']
-            kwargs = sa_info_list[0]['kwargs']
+            method = sa_req_list[0]['method']
+            resource = sa_req_list[0]['resource']
+            kwargs = sa_req_list[0]['kwargs']
             request_info = kwargs['kwargs']['request_info']
             del kwargs['kwargs']['request_info']
-            context = sa_info_list[0]['context']
+            context = sa_req_list[0]['context']
             service_type = kwargs.get('kwargs').get('service_type')
 
             # Get the service driver and invoke its method
@@ -161,13 +202,13 @@ class AgentBaseEventHandler(object):
             # Remove the processed request data blob from the service
             # information list. APIs will always process first data blob in
             # the request.
-            sa_info_list.pop(0)
+            sa_req_list.pop(0)
 
             # Invoke base class method to process further data blobs in the
             # request
-            self._rpcmgr.forward_request(sa_info_list, notification_data)
+            self.rpcmgr.process_request(sa_req_list, notification_data)
         else:
-            self._notification(notification_data)
+            self.notify._notification(notification_data)
             raise Exception(msg)
 
 
@@ -176,6 +217,6 @@ def init_agent_complete(cm, sc, conf):
     pass
 
 
-def init_agent(cm, sc, conf, nqueue):
+def init_agent(cm, sc, conf):
     """Placeholder method to satisfy configurator module agent loading."""
     pass
