7ee716978532f35c51e31fb03b7a51983e2d2bbf
Deepak <deepak.santhos@oneconvergence.com>
2016-03-16 18:18:17 +0530

    addressed review comments

--- gbpservice/nfp/configurator/lib/constants.py
+++ gbpservice/nfp/configurator/lib/constants.py
@@ -4,3 +4,6 @@ supported_service_types = ['firewall', 'vpn', 'loadbalancer', None]
 invalid_service_type = 'invalid'
 SUCCESS = 'SUCCESS'
 FAILED = 'FAILED'
+ORCHESTRATOR = 'orchestrator'
+STASH_EVENT = 'STASH_EVENT'
+PROCESS_BATCH = 'PROCESS_BATCH'
