04bc4b6d2061d7e9948ad8ce4119e92a1d33ae16
mageshgv-oc <magesh.gv@oneconvergence.com>
2016-03-17 12:48:50 +0530

    Revert "Fix merge conflict ? in previous commits"

--- gbpservice/nfp/lifecycle_manager/modules/device_lifecycle_manager.py
+++ gbpservice/nfp/lifecycle_manager/modules/device_lifecycle_manager.py
@@ -414,10 +414,9 @@ class DeviceLifeCycleHandler(object):
         # TODO: Get these values from SLCM, it should be available in service
         # profile.
         device_data['compute_policy'] = 'nova'
-        if nsi_port_info[0]['port_model'] == nfp_constants.GBP_PORT:
-            device_data['network_model'] = nfp_constants.GBP_NETWORK
-        else:
-            device_data['network_model'] = nfp_constants.NEUTRON_NETWORK
+        # To get the network mode, fetch it from port_info
+        device_data['network_policy'] = nsi_port_info[0]['port_policy'].lower()
+
         return device_data
 
     def _get_nsf_db_resource(self, resource_name, resource_id):
