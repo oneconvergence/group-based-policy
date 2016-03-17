aa3f693daab68b094fb917c6e64aa8d19db065f6
Deepak <deepak.santhos@oneconvergence.com>
2016-03-17 19:35:29 +0530

    synced latest configurator code

--- gbpservice/nfp/configurator/lib/generic_config_constants.py
+++ gbpservice/nfp/configurator/lib/generic_config_constants.py
@@ -1,15 +1,14 @@
-# One Convergence, Inc. CONFIDENTIAL
-# Copyright (c) 2012-2016, One Convergence, Inc., USA
-# All Rights Reserved.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-# All information contained herein is, and remains the property of
-# One Convergence, Inc. and its suppliers, if any. The intellectual and
-# technical concepts contained herein are proprietary to One Convergence,
-# Inc. and its suppliers.
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Dissemination of this information or reproduction of this material is
-# strictly forbidden unless prior written permission is obtained from
-# One Convergence, Inc., USA
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 DRIVERS_DIR = 'gbpservice.nfp.configurator.drivers'
 SERVICE_TYPE = 'generic_config'
