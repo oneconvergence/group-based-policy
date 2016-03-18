e681d56b485183907fb12d5426f79bee355b471e
“Dhuldev-Valekar” <dhuldev.valekar@oneconvergence.com>
2016-03-18 11:47:30 +0530

    Added Haproxy support

--- gbpservice/nfp/Automation-Scripts/NFP_SCRIPTS/nfp_setup.sh
+++ gbpservice/nfp/Automation-Scripts/NFP_SCRIPTS/nfp_setup.sh
@@ -2,7 +2,8 @@ TOP_DIR=$2
 NFP_SCRIPTS_DIR=$1
 ConfiguratorQcow2Image=$3
 VyosQcow2Image=$4
+HaproxyQcow2Image=$5
 sudo bash $NFP_SCRIPTS_DIR/configure_nfp_gbp_params.sh $NFP_SCRIPTS_DIR $TOP_DIR  
  
-sudo bash $NFP_SCRIPTS_DIR/wakeup_service.sh $ConfiguratorQcow2Image $VyosQcow2Image $NFP_SCRIPTS_DIR $TOP_DIR
+sudo bash $NFP_SCRIPTS_DIR/wakeup_service.sh $ConfiguratorQcow2Image $VyosQcow2Image $NFP_SCRIPTS_DIR $TOP_DIR $HaproxyQcow2Image
 
