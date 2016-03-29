1444b2d0d812e4433d218276df78da8ac77483a8
rajendramachani <rajendra.machani@oneconvergence.com>
2016-03-29 19:25:52 +0530

    NFP service pluigns
    (1) Firewall plugin
    (2) Loadbalancer plugin driver
    
    LBaaS plugin driver, integration testing with the RPC API version 2.0 is pending

---
+++ gbpservice/nfp/service_plugins/firewall/nfp_fwaas_plugin.py
@@ -0,0 +1,22 @@
+from oslo_config import cfg
+
+from gbpservice.nfp.config_orchestrator.agent import topics
+import neutron_fwaas.extensions
+from neutron_fwaas.services.firewall import fwaas_plugin as ref_fw_plugin
+
+
+class NFPFirewallPlugin(ref_fw_plugin.FirewallPlugin):
+    def __init__(self):
+        # Monkey patch L3 agent topic
+        # L3 agent was where reference firewall agent runs
+        # patch that topic to the NFP firewall agent's topic name
+        ref_fw_plugin.topics.L3_AGENT = topics.FW_NFP_CONFIGAGENT_TOPIC
+
+        # Ensure neutron fwaas extensions are loaded
+        ext_path = neutron_fwaas.extensions.__path__[0]
+        if ext_path not in cfg.CONF.api_extensions_path.split(':'):
+            cfg.CONF.set_override(
+                'api_extensions_path',
+                cfg.CONF.api_extensions_path + ':' + ext_path)
+
+        super(NFPFirewallPlugin, self).__init__()
---
+++ gbpservice/nfp/service_plugins/loadbalancer/drivers/nfp_lbaas_plugin_driver.py
@@ -0,0 +1,22 @@
+from gbpservice.nfp.config_orchestrator.agent import topics
+from gbpservice.nfp.configurator.drivers.loadbalancer.v1.haproxy import (
+    haproxy_lb_driver
+)
+from neutron_lbaas.services.loadbalancer.drivers.common import (
+    agent_driver_base as adb
+)
+
+
+class HaproxyOnVMPluginDriver(adb.AgentDriverBase):
+    device_driver = haproxy_lb_driver.DRIVER_NAME
+
+    def __init__(self, plugin):
+        # Monkey patch LB agent topic and LB agent type
+        adb.topics.LOADBALANCER_AGENT = topics.LB_NFP_CONFIGAGENT_TOPIC
+        adb.q_const.AGENT_TYPE_LOADBALANCER = 'NFP Loadbalancer agent'
+
+        super(HaproxyOnVMPluginDriver, self).__init__(plugin)
+
+        # TODO (RPM): RPC API version 2.0 is not integration tested
+        # Till then this is a workaround to use RPC API 1.0
+        self.agent_rpc.client.target.version = '1.0'
