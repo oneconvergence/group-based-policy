1533794329db3d13a34a0ffb88ffbf8563823ad7
“AkashDeepSrivastava” <akash.deep@oneconvergence.com>
2016-04-04 19:22:06 +0530

    Ran config_orchestrator_run_test and fixed rpc version error

--- gbpservice/nfp/config_orchestrator/agent/loadbalancer.py
+++ gbpservice/nfp/config_orchestrator/agent/loadbalancer.py
@@ -10,15 +10,21 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from gbpservice.nfp.config_orchestrator.agent import common
+from gbpservice.nfp.lib import transport
 from neutron_lbaas.db.loadbalancer import loadbalancer_db
-from gbpservice.nfp.config_orchestrator.agent.common import *
-from gbpservice.nfp.lib.transport import *
+from oslo_log import helpers as log_helpers
+import oslo_messaging as messaging
 
-LOG = logging.getLogger(__name__)
+
+"""
+RPC handler for Loadbalancer service
+"""
+
 
 class LbAgent(loadbalancer_db.LoadBalancerPluginDb):
-    RPC_API_VERSION = '2.0'
-    target = target.Target(version=RPC_API_VERSION)
+    RPC_API_VERSION = '1.0'
+    target = messaging.Target(version=RPC_API_VERSION)
 
     def __init__(self, conf, sc):
         self._conf = conf
@@ -26,48 +32,68 @@ class LbAgent(loadbalancer_db.LoadBalancerPluginDb):
         super(LbAgent, self).__init__()
 
     def _post(self, context, tenant_id, name, **kwargs):
+
+        # Collecting db entry required by configurator.
         db = self._context(context, tenant_id)
+        # Addind service_info to neutron context and sending
+        # dictionary format to the configurator.
         context_dict = context.to_dict()
         context_dict.update({'service_info': db})
         kwargs.update({'context': context_dict})
-        body = prepare_request_data(name, kwargs, "loadbalancer")
-        send_request_to_configurator(self._conf, context, body, "CREATE")
+        body = common.prepare_request_data(name, kwargs, "loadbalancer")
+        transport.send_request_to_configurator(self._conf,
+                                               context, body,
+                                               "CREATE")
 
     def _delete(self, context, tenant_id, name, **kwargs):
+
+        # Collecting db entry required by configurator.
         db = self._context(context, tenant_id)
+        # Addind service_info to neutron context and sending
+        # dictionary format to the configurator.
         context_dict = context.to_dict()
         context_dict.update({'service_info': db})
         kwargs.update({'context': context_dict})
-        body = prepare_request_data(name, kwargs, "loadbalancer")
-        send_request_to_configurator(self._conf, context, body, "DELETE")
+        body = common.prepare_request_data(name, kwargs, "loadbalancer")
+        transport.send_request_to_configurator(self._conf,
+                                               context, body,
+                                               "DELETE")
 
+    @log_helpers.log_method_call
     def create_vip(self, context, vip):
         self._post(context, vip['tenant_id'], 'vip', vip=vip)
 
+    @log_helpers.log_method_call
     def delete_vip(self, context, vip):
         self._delete(context, vip['tenant_id'], 'vip', vip=vip)
 
+    @log_helpers.log_method_call
     def create_pool(self, context, pool, driver_name):
         self._post(
             context, pool['tenant_id'],
             'pool', pool=pool, driver_name=driver_name)
 
+    @log_helpers.log_method_call
     def delete_pool(self, context, pool):
         self._delete(context, pool['tenant_id'], 'pool', pool=pool)
 
+    @log_helpers.log_method_call
     def create_member(self, context, member):
         self._post(context, member['tenant_id'], 'member', member=member)
 
+    @log_helpers.log_method_call
     def delete_member(self, context, member):
         self._delete(
             context, member['tenant_id'], 'member',
             member=member)
 
+    @log_helpers.log_method_call
     def create_pool_health_monitor(self, context, health_monitor, pool_id):
         self._post(context, health_monitor[
             'tenant_id'], 'pool_health_monitor',
             health_monitor=health_monitor, pool_id=pool_id)
 
+    @log_helpers.log_method_call
     def delete_pool_health_monitor(self, context, health_monitor, pool_id):
         self._delete(
             context, health_monitor['tenant_id'], 'pool_health_monitor',
@@ -82,11 +108,10 @@ class LbAgent(loadbalancer_db.LoadBalancerPluginDb):
         return db
 
     def _get_core_context(self, context, filters):
-        core_context_dict = get_core_context(context, filters, self._conf.host)
+        core_context_dict = common.get_core_context(context,
+                                                    filters,
+                                                    self._conf.host)
         del core_context_dict['routers']
-        # Warning
-        #del core_context_dict['subnets']
-
         return core_context_dict
 
     def _get_lb_context(self, context, filters):
