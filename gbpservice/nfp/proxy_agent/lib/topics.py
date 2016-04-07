508d0fd426e380c1ec32125d93dd1be3ed01ab67
Ashutosh Mishra <ashutosh.mishra@oneconvergence.com>
2016-04-07 13:06:10 +0530

    Added missed topic changes

--- gbpservice/nfp/proxy_agent/lib/topics.py
+++ gbpservice/nfp/proxy_agent/lib/topics.py
@@ -10,9 +10,22 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-CONFIG_AGENT_PROXY = 'nfp-config-agent-proxy'
+
+
+
+FW_NFP_CONFIGAGENT_TOPIC = 'nfp-firewall-agent'
+LB_NFP_CONFIGAGENT_TOPIC = 'nfp-lbaas-agent'
+VPN_NFP_CONFIGAGENT_TOPIC = 'nfp-vpn_agent'
+
+FW_NFP_PROXY_TOPIC = 'nfp-proxy-firewall-agent'
+LB_NFP_PROXY_TOPIC = 'nfp-proxy-lbaas-agent'
+VPN_NFP_PROXY_TOPIC = 'nfp-proxy-vpn_agent'
+
 FW_NFP_PLUGIN_TOPIC = 'q-firewall-plugin'
 LB_NFP_PLUGIN_TOPIC = 'n-lbaas-plugin'
 VPN_NFP_PLUGIN_TOPIC = 'vpn_plugin'
 DEVICE_ORCHESTRATOR_TOPIC = 'nfp-configurator-ndo'
 SERVICE_ORCHESTRATOR_TOPIC = 'nfp-configurator-nso'
+
+
+CONFIG_AGENT_PROXY = 'nfp-config-agent-proxy'
