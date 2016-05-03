1d2272c396c52606d01fc8a20bbabce4a15839ba
Suresh <suresh.dharavath@oneconvergence.com>
2016-05-03 15:53:30 +0530

    vpn review comments addressed

--- gbpservice/nfp/service_plugins/vpn/drivers/nfp_vpnaas_driver.py
+++ gbpservice/nfp/service_plugins/vpn/drivers/nfp_vpnaas_driver.py
@@ -1,11 +1,23 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import socket
 
+from gbpservice.nfp.config_orchestrator.agent import topics
 from neutron_lib import exceptions
 from neutron.common import rpc as n_rpc
 from neutron.db import agents_db
 from neutron.db import agentschedulers_db
 from neutron import manager
-from gbpservice.nfp.config_orchestrator.agent import topics
 from neutron_vpnaas.services.vpn.plugin import VPNPlugin
 from neutron_vpnaas.services.vpn import service_drivers
 from neutron_vpnaas.services.vpn.service_drivers import base_ipsec
