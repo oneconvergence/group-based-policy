956d825ce2a2756298e292da220fb6318fa09e86
pkharat <pritam.kharat@oneconvergence.com>
2016-05-17 16:19:29 +0530

    Fixed pecan server unit test cases and pep8 warnings

--- gbpservice/nfp/configurator/api/base_controller.py
+++ gbpservice/nfp/configurator/api/base_controller.py
@@ -31,7 +31,8 @@ class ZipperHook(PecanHook):
             state.request.json_body = body
             state.request.content_type = "application/json"
         except Exception as e:
-            LOG.error("Failed to process data ,Reason: %s", e)
+            msg = ("Failed to process data ,Reason: %s", e)
+            LOG.error(msg)
 
     def after(self, state):
         data = state.response.body
