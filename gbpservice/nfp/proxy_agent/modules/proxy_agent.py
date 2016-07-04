335c15c19094b04f8f9ce2ebbb077ce31853617a
Rahul Shikhare <rahul.shikhare@oneconvergence.com>
2016-07-04 17:21:41 +0530

    UP Time bug fix.

--- gbpservice/nfp/proxy_agent/modules/proxy_agent.py
+++ gbpservice/nfp/proxy_agent/modules/proxy_agent.py
@@ -69,20 +69,15 @@ class RpcHandler(object):
         """Method of rpc handler for create_network_function_config.
         Return: Http Response.
         """
-        if 'eventid' in body and body['eventid'] == 'NFP_UP_TIME':
-            transport.send_request_to_configurator(self._conf,
-                                                   context, body,
-                                                   "CREATE")
-        else:
-            body_info = body.get('info')
-            body_context = body_info.get('context')
-            logging_context = body_context.get('logging_context', {})
-            nfp_logging.store_logging_context(**logging_context)
+        body_info = body.get('info')
+        body_context = body_info.get('context')
+        logging_context = body_context.get('logging_context', {})
+        nfp_logging.store_logging_context(**logging_context)
 
-            transport.send_request_to_configurator(self._conf,
-                                                   context, body,
-                                                   "CREATE")
-            nfp_logging.clear_logging_context()
+        transport.send_request_to_configurator(self._conf,
+                                               context, body,
+                                               "CREATE")
+        nfp_logging.clear_logging_context()
 
     @log_helpers.log_method_call
     def delete_network_function_config(self, context, body):
@@ -168,13 +163,19 @@ class RpcHandler(object):
         """Method of rpc handler for create_service.
         Return: Http Response.
         """
-        body_info = body.get('info')
-        body_context = body_info.get('context')
-        logging_context = body_context.get('logging_context', {})
-        nfp_logging.store_logging_context(**logging_context)
+        if 'eventid' in body and body['eventid'] == 'NFP_UP_TIME':
+            transport.send_request_to_configurator(self._conf,
+                                                   context, body,
+                                                   "CREATE",
+                                                   network_function_event=True)
+        else:
+            body_info = body.get('info')
+            body_context = body_info.get('context')
+            logging_context = body_context.get('logging_context', {})
+            nfp_logging.store_logging_context(**logging_context)
 
-        transport.send_request_to_configurator(self._conf,
-                                               context, body,
-                                               "CREATE",
-                                               network_function_event=True)
-        nfp_logging.clear_logging_context()
+            transport.send_request_to_configurator(self._conf,
+                                                   context, body,
+                                                   "CREATE",
+                                                   network_function_event=True)
+            nfp_logging.clear_logging_context()
