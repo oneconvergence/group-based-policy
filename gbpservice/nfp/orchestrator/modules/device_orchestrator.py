a065161da82feff92c31905d0a5c72dfc48d1a07
rayalaman <ravi.yalamanchili@oneconvergence.com>
2016-07-06 22:12:09 -0700

    libery fixes

--- gbpservice/nfp/orchestrator/modules/device_orchestrator.py
+++ gbpservice/nfp/orchestrator/modules/device_orchestrator.py
@@ -558,7 +558,7 @@ class DeviceOrchestrator(nfp_api.NfpEventHandler):
 
         t_ports = []
         for ptg in [consumer, provider]:
-            if port_type in ptg.keys():
+            if ptg['port'] and port_type in ptg.keys():
                 t_ports.append({
                                 'id': ptg[port_type].get('id'),
                                 'port_classification': ptg.get(
