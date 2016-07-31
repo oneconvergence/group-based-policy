69efd94d92125373191cf2bf7101248bd4802d65
rayalaman <ravi.yalamanchili@oneconvergence.com>
2016-07-31 13:35:16 -0700

    - fixed bug with liberty porting of neutron lbaas topics

--- gbpservice/contrib/nfp/service_plugins/loadbalancer/drivers/nfp_lbaas_plugin_driver.py
+++ gbpservice/contrib/nfp/service_plugins/loadbalancer/drivers/nfp_lbaas_plugin_driver.py
@@ -16,6 +16,8 @@ from gbpservice.contrib.nfp.configurator.drivers.loadbalancer.v1.haproxy\
 from neutron_lbaas.services.loadbalancer.drivers.common import (
     agent_driver_base as adb
 )
+from neutron.common import topics as n_topics
+from neutron.common import constants as n_const
 
 
 class HaproxyOnVMPluginDriver(adb.AgentDriverBase):
@@ -23,7 +25,9 @@ class HaproxyOnVMPluginDriver(adb.AgentDriverBase):
 
     def __init__(self, plugin):
         # Monkey patch LB agent topic and LB agent type
-        adb.l_const.LOADBALANCER_AGENT = topics.LB_NFP_CONFIGAGENT_TOPIC
-        adb.q_const.AGENT_TYPE_LOADBALANCER = 'NFP Loadbalancer agent'
+        #adb.l_const.LOADBALANCER_AGENT = topics.LB_NFP_CONFIGAGENT_TOPIC
+        #adb.q_const.AGENT_TYPE_LOADBALANCER = 'NFP Loadbalancer agent'
+        n_topics.LOADBALANCER_AGENT = topics.LB_NFP_CONFIGAGENT_TOPIC
+        n_const.AGENT_TYPE_LOADBALANCER = 'NFP Loadbalancer agent'
 
         super(HaproxyOnVMPluginDriver, self).__init__(plugin)
