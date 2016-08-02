760dd889bcd14841ea6fe583d3737fa9120f70f1
DhuldevValekar <dhuldev.valekar@oneconvergence.com>
2016-08-02 14:42:34 +0530

    Removed screen from docker

--- gbpservice/contrib/nfp/configurator/run.sh
+++ gbpservice/contrib/nfp/configurator/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 service rabbitmq-server start
-screen -dmS "configurator" /usr/bin/python2 /usr/bin/nfp --config-file=/etc/nfp_configurator.ini --log-file=/var/log/nfp/nfp_configurator.log
+nohup /usr/bin/python2 /usr/bin/nfp --config-file=/etc/nfp_configurator.ini --log-file=/var/log/nfp/nfp_configurator.log  &> /dev/null &
 cd /usr/local/lib/python2.7/dist-packages/gbpservice/nfp/pecan/api/
 python setup.py develop
-screen -dmS  "pecan" pecan configurator_decider config.py --mode  advanced
+nohup pecan configurator_decider config.py --mode  advanced &> /dev/null &
 /bin/bash
 
